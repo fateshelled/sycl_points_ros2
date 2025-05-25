5fc5bb744aa800efb23c2705677f358e5abaaa9c
fateshelled <53618876+fateshelled@users.noreply.github.com>
2025-05-25 20:56:36 +0900

    update node

--- include/sycl_points_ros2/lidar_odometry.hpp
+++ include/sycl_points_ros2/lidar_odometry.hpp
@@ -2,15 +2,20 @@
 
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/point_cloud2.hpp>
+#include <sycl_points/algorithms/knn_search.hpp>
 #include <sycl_points/algorithms/preprocess_filter.hpp>
 #include <sycl_points/algorithms/registration.hpp>
 #include <sycl_points/algorithms/voxel_downsampling.hpp>
 #include <sycl_points/utils/sycl_utils.hpp>
+#include <tf2_ros/transform_broadcaster.h>
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <nav_msgs/msg/odometry.hpp>
 
 namespace sycl_points {
 namespace ros2 {
 class LiDAROdometryNode : public rclcpp::Node {
 public:
+EIGEN_MAKE_ALIGNED_OPERATOR_NEW
     LiDAROdometryNode(const rclcpp::NodeOptions &options);
     ~LiDAROdometryNode();
 
@@ -18,17 +23,29 @@ private:
     rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr sub_pc_;
     rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pub_preprocessed_;
     rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pub_submap_;
+    rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr pub_pose_;
+    rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr pub_odom_;
+    std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
+
     sycl_utils::DeviceQueue::Ptr queue_ptr_;
 
     PointCloudShared::Ptr scan_pc_ = nullptr;
     PointCloudShared::Ptr preprocessed_pc_ = nullptr;
+    PointCloudShared::Ptr submap_pc_ = nullptr;
+    algorithms::knn_search::KDTree::Ptr submap_tree_ = nullptr;
 
     algorithms::filter::PreprocessFilter::Ptr preprocess_filter_;
     algorithms::filter::VoxelGrid::Ptr voxel_filter_;
     algorithms::registration::RegistrationGICP::Ptr gicp_;
     algorithms::registration::RegistrationParams gicp_param_;
 
+    sycl_utils::events build_submap_events_;
+
+    Eigen::Isometry3f odom_;
+    Eigen::Isometry3f last_keyframe_pose_;
+
     void point_cloud_callback(const sensor_msgs::msg::PointCloud2::ConstSharedPtr msg);
+    void publish_odom(const std_msgs::msg::Header& header, const Eigen::Isometry3f& odom);
 };
 }  // namespace ros2
 }  // namespace sycl_points
--- src/lidar_odometry.cpp
+++ src/lidar_odometry.cpp
@@ -1,6 +1,7 @@
 #include "sycl_points_ros2/lidar_odometry.hpp"
 
 #include <rclcpp_components/register_node_macro.hpp>
+#include <sycl_points/algorithms/covariance.hpp>
 #include <sycl_points/ros2/convert.hpp>
 #include <sycl_points/utils/time_utils.hpp>
 
@@ -18,9 +19,11 @@ LiDAROdometryNode::LiDAROdometryNode(const rclcpp::NodeOptions& options) : rclcp
 
     // initialize params
     const float voxel_size = 1.00f;
-    this->gicp_param_.max_iterations = 10;
-    this->gicp_param_.max_correspondence_distance = 1.0f;
+    this->gicp_param_.max_iterations = 20;
+    this->gicp_param_.max_correspondence_distance = 2.0f;
     this->gicp_param_.verbose = false;
+    this->odom_.setIdentity();
+    this->last_keyframe_pose_.setIdentity();
 
     // Point cloud processor
     this->preprocess_filter_ = std::make_shared<algorithms::filter::PreprocessFilter>(*this->queue_ptr_);
@@ -29,13 +32,15 @@ LiDAROdometryNode::LiDAROdometryNode(const rclcpp::NodeOptions& options) : rclcp
 
     // pub/sub
     this->sub_pc_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
-        "points", rclcpp::SensorDataQoS(),
-        std::bind(&LiDAROdometryNode::point_cloud_callback, this, std::placeholders::_1));
+        "points", rclcpp::QoS(10), std::bind(&LiDAROdometryNode::point_cloud_callback, this, std::placeholders::_1));
 
     this->pub_preprocessed_ =
         this->create_publisher<sensor_msgs::msg::PointCloud2>("sycl_lo/preprocessed", rclcpp::SensorDataQoS());
-    this->pub_submap_ =
-        this->create_publisher<sensor_msgs::msg::PointCloud2>("sycl_lo/submap", rclcpp::SensorDataQoS());
+    this->pub_submap_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("sycl_lo/submap", rclcpp::QoS(1));
+
+    this->pub_odom_ = this->create_publisher<nav_msgs::msg::Odometry>("sycl_lo/odom", rclcpp::QoS(10));
+    this->pub_pose_ = this->create_publisher<geometry_msgs::msg::PoseStamped>("sycl_lo/pose", rclcpp::QoS(10));
+    this->tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
 }
 
 /// @brief destructor
@@ -45,30 +50,177 @@ void LiDAROdometryNode::point_cloud_callback(const sensor_msgs::msg::PointCloud2
     double dt_from_ros2_msg = 0.0;
     time_utils::measure_execution([&]() { return fromROS2msg(*this->queue_ptr_, *msg, this->scan_pc_); },
                                   dt_from_ros2_msg);
+
+    // preprocess
     double dt_voxel_downsampling = 0.0;
     time_utils::measure_execution(
         [&]() {
+            const float box_min = 2.0f;
+            const float box_max = 50.0f;
+
             if (this->preprocessed_pc_ == nullptr) {
                 this->preprocessed_pc_ = std::make_shared<PointCloudShared>(*this->queue_ptr_);
             }
+            this->preprocess_filter_->box_filter(*this->scan_pc_, box_min, box_max);
             this->voxel_filter_->downsampling(*this->scan_pc_, *this->preprocessed_pc_);
         },
         dt_voxel_downsampling);
 
-    double dt_to_ros2_msg = 0.0;
-    auto pub_msg = time_utils::measure_execution([&]() { return toROS2msg(*this->preprocessed_pc_, msg->header); },
-                                                 dt_to_ros2_msg);
+    // compute covariances
+    double dt_covariance = 0.0;
+    const auto src_tree = time_utils::measure_execution(
+        [&]() {
+            const size_t covariance_neighbor_num = 20;
 
-    double dt_publish = 0.0;
-    if (this->pub_preprocessed_->get_subscription_count() > 0) {
-        time_utils::measure_execution([&]() { return this->pub_preprocessed_->publish(std::move(pub_msg)); },
-                                      dt_publish);
+            const auto tree =
+                sycl_points::algorithms::knn_search::KDTree::build(*this->queue_ptr_, *this->preprocessed_pc_);
+            algorithms::covariance::compute_covariances(*tree, *this->preprocessed_pc_, covariance_neighbor_num);
+            algorithms::covariance::covariance_update_plane(*this->preprocessed_pc_);
+            return tree;
+        },
+        dt_covariance);
+
+    if (this->submap_pc_ == nullptr) {
+        // copy to submap
+        this->submap_pc_ = std::make_shared<PointCloudShared>(*this->preprocessed_pc_);
+        this->submap_tree_ = src_tree;
+        return;
     }
 
-    RCLCPP_INFO(this->get_logger(), "fromROS2msg:       %8.3f us", dt_from_ros2_msg);
-    RCLCPP_INFO(this->get_logger(), "VoxelDownsampling: %8.3f us", dt_voxel_downsampling);
-    RCLCPP_INFO(this->get_logger(), "toROS2msg:         %8.3f us", dt_to_ros2_msg);
-    RCLCPP_INFO(this->get_logger(), "publish:           %8.3f us", dt_publish);
+    // Registration
+    double dt_registration = 0.0;
+    const auto reg_result = time_utils::measure_execution(
+        [&]() {
+            const auto result = this->gicp_->align(*this->preprocessed_pc_, *this->submap_pc_, *this->submap_tree_,
+                                                   this->odom_.matrix());
+
+            if (result.converged) {
+                // update odometry
+                this->odom_ = result.T;
+            }
+
+            return result;
+        },
+        dt_registration);
+
+    // Build submap
+    double dt_build_submap = 0.0;
+    time_utils::measure_execution(
+        [&]() {
+            if (!reg_result.converged) {
+                return;
+            }
+            // calculate delta pose
+            const auto delta_pose = this->last_keyframe_pose_.inverse() * reg_result.T;
+
+            // calculate moving distance and angle
+            const auto distance = delta_pose.translation().norm();
+            const auto angle = Eigen::AngleAxisf(delta_pose.rotation()).angle() * (float)(180.0 / M_PI);
+
+            const float keyframe_distance_threshold = 2.0f;
+            const float keyframe_angle_threshold_degrees = 30.0f;
+            const size_t max_submap_size = 20000;
+
+            // update submap
+            if (distance >= keyframe_distance_threshold || angle >= keyframe_angle_threshold_degrees) {
+                this->last_keyframe_pose_ = reg_result.T;
+
+                const auto aligned = this->gicp_->get_aligned_point_cloud();
+                if (aligned == nullptr) {
+                    RCLCPP_ERROR(this->get_logger(), "aligned pc is nullptr");
+                }
+                // add new points
+                this->submap_pc_->extend(*aligned);
+
+                // remove old points
+                if (this->submap_pc_->size() > max_submap_size) {
+                    this->submap_pc_->erase(0, this->submap_pc_->size() - max_submap_size);
+                    RCLCPP_ERROR(this->get_logger(), "erase %ld ", this->submap_pc_->size());
+                }
+
+                // this->submap_tree_ =
+                //     sycl_points::algorithms::knn_search::KDTree::build(*this->queue_ptr_, *this->submap_pc_);
+                this->queue_ptr_->ptr
+                    ->submit([&](sycl::handler& h) {
+                        h.host_task([&]() {
+                            this->submap_tree_ = sycl_points::algorithms::knn_search::KDTree::build(*this->queue_ptr_,
+                                                                                                    *this->submap_pc_);
+                        });
+                    })
+                    .wait();
+            }
+        },
+        dt_build_submap);
+
+    // publish ROS2 message
+    double dt_to_ros2_msg = 0.0;
+    auto pub_msgs = time_utils::measure_execution(
+        [&]() {
+            auto preprocessed_msg = toROS2msg(*this->preprocessed_pc_, msg->header);
+
+            auto submap_msg = toROS2msg(*this->submap_pc_, msg->header);
+            submap_msg->header.frame_id = "odom";
+
+            return std::make_tuple(std::move(preprocessed_msg), std::move(submap_msg));
+        },
+        dt_to_ros2_msg);
+
+    double dt_publish = 0.0;
+    time_utils::measure_execution(
+        [&]() {
+            this->publish_odom(msg->header, this->odom_);
+            if (this->pub_preprocessed_->get_subscription_count() > 0) {
+                this->pub_preprocessed_->publish(std::move(std::get<0>(pub_msgs)));
+            }
+            if (this->pub_submap_->get_subscription_count() > 0) {
+                this->pub_submap_->publish(std::move(std::get<1>(pub_msgs)));
+            }
+        },
+        dt_publish);
+
+    RCLCPP_INFO(this->get_logger(), "fromROS2msg:         %9.2f us", dt_from_ros2_msg);
+    RCLCPP_INFO(this->get_logger(), "VoxelDownsampling:   %9.2f us", dt_voxel_downsampling);
+    RCLCPP_INFO(this->get_logger(), "compute Covariances: %9.2f us", dt_covariance);
+    RCLCPP_INFO(this->get_logger(), "Registration:        %9.2f us", dt_registration);
+    RCLCPP_INFO(this->get_logger(), "Build submap:        %9.2f us", dt_build_submap);
+    RCLCPP_INFO(this->get_logger(), "toROS2msg:           %9.2f us", dt_to_ros2_msg);
+    RCLCPP_INFO(this->get_logger(), "publish:             %9.2f us", dt_publish);
+}
+
+void LiDAROdometryNode::publish_odom(const std_msgs::msg::Header& header, const Eigen::Isometry3f& odom) {
+    geometry_msgs::msg::TransformStamped tf;
+    tf.header.stamp = header.stamp;
+    tf.header.frame_id = "odom";
+    tf.child_frame_id = "base_link";
+
+    const auto odom_trans = odom.translation();
+    tf.transform.translation.x = odom_trans.x();
+    tf.transform.translation.y = odom_trans.y();
+    tf.transform.translation.z = odom_trans.z();
+    const Eigen::Quaternionf odom_quat(odom.rotation());
+    tf.transform.rotation.x = odom_quat.x();
+    tf.transform.rotation.y = odom_quat.y();
+    tf.transform.rotation.z = odom_quat.z();
+    tf.transform.rotation.w = odom_quat.w();
+    this->tf_broadcaster_->sendTransform(tf);
+
+    geometry_msgs::msg::PoseStamped pose;
+    pose.header = tf.header;
+    pose.pose.position.x = odom_trans.x();
+    pose.pose.position.y = odom_trans.y();
+    pose.pose.position.z = odom_trans.z();
+    pose.pose.orientation.x = odom_quat.x();
+    pose.pose.orientation.y = odom_quat.y();
+    pose.pose.orientation.z = odom_quat.z();
+    pose.pose.orientation.w = odom_quat.w();
+    this->pub_pose_->publish(pose);
+
+    nav_msgs::msg::Odometry odom_msg;
+    odom_msg.header = tf.header;
+    odom_msg.child_frame_id = tf.child_frame_id;
+    odom_msg.pose.pose.position = pose.pose.position;
+    odom_msg.pose.pose.orientation = pose.pose.orientation;
+    this->pub_odom_->publish(odom_msg);
 }
 
 }  // namespace ros2
